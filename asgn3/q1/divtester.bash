4400f4f2f6fe941a23fa0f118438db963ba68241
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-26 19:56:35 -0400

    Create divtester.bash

---
+++ asgn3/q1/divtester.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# this thing should have a bunch of testers
+
+# first test: display "Test 1: not divisible & not increasing"
+# then run program with numbers "10 5 7" (use redirection to replace keyboard input)
+# (redirection: gcc divisible.c 2> smth?)
+# Following output from command itself, display "Expected output message: Not divisible & Not increasing
+
+# second test: display "Test 2: divisible & not increasing"
+# run program with numbers "5 20 10".
+# Following output from command itself, depending on error code, display "Exited with expected return code: 2"
+# or "Exited with unexpected return code: X (Expected 2)"; X is the error code in your C program
+
+# can put as many tests as you want.
