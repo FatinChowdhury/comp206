88d884028928079df0fd12ef6874d8bd9ad46036
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-30 22:48:05 -0400

    Update anagram.c

--- asgn3/q2/anagram.c
+++ asgn3/q2/anagram.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+int main(int argc, char *argv[]){
+        for (int n=0; n<argc; n++){
+                if (argv[1] == argv[2]){
+                        return 0;
+                else
+                        return 1;
+        }
+
+
+
+
+        return 0;
+}
