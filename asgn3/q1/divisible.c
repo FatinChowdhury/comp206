f07ceeb6318ef8a3d57db270abb45bd155e62c95
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-10-30 20:25:11 -0400

    Update divisible.c

--- asgn3/q1/divisible.c
+++ asgn3/q1/divisible.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
-int main(int argc, int *argv[]){
-        puts("Please enter three numbers: ");
-        scanf("%d", argv);
+// 1. asks for 3 ints
+// 2. checks if (1st int)|(2nd & 3rd ints)
+// 3. the 3 ints are in increasing order
+//
+// Program must:
+// -1 return 0; if all conditions satisfied
+// prints Divisible & Increasing to screen
+//
+// -2 return 1; if 2 == false && 3 == true
+// prints Not Divisible & Increasing to screen
+//
+// -3 return 2; if 2 == true && 3 == false
+// prints Divisible & Not Increasing
+//
+// -4 return 3; if 2 == false && 3 == false
+// prints Not Divisible && Not Increasing
+//
+// program terminates after doing this one time.
+//
+int main(){
+        int num1, num2, num3;
 
-        for (int i=0; i<3; i++){
-                if (argv[1] % argv[0] == 0 && argv[2] % argv[0] == 0){
+        printf("Please input three numbers: ");
+        scanf("%d %d %d", &num1, &num2, &num3);
 
-                        if (argv[0] < argv[1] && argv[1] < argv[2]){
-                                puts("Divisible & Increasing");
+        if (num1 != 0 && num2 % num1 == 0 && num3 % num1 == 0){
+
+                if (num1 < num2 && num2 < num3){
+                        puts("Divisible & Increasing");
                                 return 0;
-                        }
-                        else {
-                                puts("Divisible & Not Increasing");
-                                return 2;
-                        }
                 }
-                else if (argv[1] % argv[0] != 0 || argv[2] % argv[0] != 0){
-                        if (argv[0] < argv[1] && argv[1] < argv[2]){
-                                puts("Not Divisible & Increasing");
-                                return 1;
-                        }
-                        else {
-                                puts("Not Divisible & Not Increasing");
-                                return 3;
-                        }
+                else {
+                        puts("Divisible & Not Increasing");
+                        return 2;
+                }
         }
+        else {
+                if (num1 < num2 && num2 < num3){
+                        puts("Not Divisible & Increasing");
+                        return 1;
+                }
+                else {
+                        puts("Not Divisible & Not Increasing");
+                        return 3;
+                }
+        }
+
+        return 0;
 }
