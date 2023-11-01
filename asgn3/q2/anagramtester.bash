ae00bfeb08cd1af8fbaa80aaccc4076b874990c3
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-11-01 14:25:44 -0400

    Update anagramtester.bash

--- asgn3/q2/anagramtester.bash
+++ asgn3/q2/anagramtester.bash
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+echo "Test 1: mary and ramy"
+output=$(./anagram mary ramy)
+echo "$output"
+echo "Expected output message: Anagram"
+
+echo "Test 2: bob and lary"
+output=$(./anagram bob lary)
+return_code=$?
+if [ $return_code -eq 1 ]; then
+        echo "$output"
+        echo "Exited with expected return code: 1"
+else
+        echo "$output"
+        echo "Exited with unexpected return code: $return_code (Expected 1)"
+fi
