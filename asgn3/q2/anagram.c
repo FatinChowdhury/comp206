b7ac5306bf6b7d6d7df53255de3eb6d84621a893
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-11-01 14:31:53 -0400

    Update anagram.c

--- asgn3/q2/anagram.c
+++ asgn3/q2/anagram.c
@@ -5,24 +5,19 @@ int areAnagrams(const char* word1, const char* word2) {
     int len1 = strlen(word1);
     int len2 = strlen(word2);
 
-    // If the lengths of the words are different, they cannot be anagrams
     if (len1 != len2) {
         return 0;
     }
 
-    int count[256] = {0}; // Assuming ASCII characters
+    int count[256] = {0};
 
-    // Count the frequency of characters in word1
     for (int i = 0; i < len1; i++) {
         count[word1[i]]++;
-    }
-
-    // Subtract the frequency of characters in word2
-    for (int i = 0; i < len2; i++) {
         count[word2[i]]--;
     }
 
-    // If all counts are zero, the words are anagrams
+
+
     for (int i = 0; i < 256; i++) {
         if (count[i] != 0) {
             return 0;
