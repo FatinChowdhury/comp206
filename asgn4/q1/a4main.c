c2dd85975df76e34644ca84d7f47954639287ae4
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2023-11-28 12:07:09 -0500

    Create a4main.c

---
+++ asgn4/q1/a4main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "a4phonebook.c"
+
+int menu(void){
+	printf("Phonebook Menu: (1)Add, (2)Find, (3)List, (4)Quit >");
+	int input;
+	scanf("%d", &input);
+	while (getchar()!='\n');
+	return input;
+}
+
+int main(){
+	char name[nameSize], birth[birthSize], phone[phoneSize];
+	int menu_input, result;
+
+	loadCSV("phonebook.csv");
+
+	while(1){
+		menu_input = menu();
+		
+		switch(menu_input){
+			// addRecord()
+			case 1:
+				printf("Enter a name: ");
+				scanf(" %[^\n]", name);
+				name[strcspn(name, "\n")] = 0;
+
+				printf("Enter a birthdate: ");
+				scanf(" %[^\n]", birth);
+                                birth[strcspn(birth, "\n")] = 0;
+
+				printf("Enter a phone number: ");
+				scanf(" %[^\n]", phone);
+                                phone[strcspn(phone, "\n")] = 0;
+
+
+				result = addRecord(name, birth, phone);
+				if (result != 0){
+					printf("Error: phonebook is full, cannot add anything.\n");
+				}
+				break;
+
+			// findRecord()
+			case 2:
+				printf("Enter a name you want to search: ");
+				scanf(" %[^\n]", name);
+				findRecord(name);
+				break;
+
+			// listRecords()
+			case 3:
+				result = listRecords();
+				if (result != 0){
+					printf("Error: phonebook is empty, there is nothing to list.\n");
+				}
+				break;
+
+			// quit program
+			case 4:
+				break;
+			
+			// if anything else
+			default:
+				printf("Invalid menu selection \n");
+		}
+		if (menu_input == 4){
+		       break;
+		}
+	}
+	result = saveCSV("phonebook.csv");
+	if (result != 0){
+		printf("Failed to save.\n");
+	}
+	printf("End of phonebook program\n");
+	return 0;
+}
